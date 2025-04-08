b234dd6fb68e87cb7ef894c62bd68dd78efe938f
1012abhi <130040653+1012abhi@users.noreply.github.com>
2025-04-09 01:29:05 +0530

    Add base path configuration and optimize build for vendor chunks in Vite setup

--- frontend-vite/vite.config.js
+++ frontend-vite/vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  base: './',
   plugins: [react()],
   server: {
     port: 3000,
@@ -15,4 +17,14 @@ export default defineConfig({
       }
     }
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'] // example
+        }
+      }
+    }
+  }
+  
 })
